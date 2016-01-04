0c3402aecff4b90099e0927f0ef26b5fd6be7971
Minori <ararai.mail@gmail.com>
2016-01-04 14:42:23 +0900

    0104_2

--- index.php
+++ index.php
@@ -2,17 +2,20 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<link type="text/css" rel="stylesheet" href="./css/style.css">
 		<title>（仮）JTRIPオフィシャルサイト</title>
+
+		<link type="text/css" rel="stylesheet" href="./css/style.css">
+		
+		<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+		<script type="text/javascript" src="./js/main.js"></script>
 	</head>
 	<body>
 		<div class="wrapper">
 			<?php include "header.php" ?>
 			<h2><img src="./img/top.jpg" width="100%" height="auto" alt="トップ画像"></h2>
-			<p>買って良かったではなく、<br>早く買えば良かった・・・</p>
 		</div>
 		
-		<?php include "footer.php"; ?>
+		<!--<?php include "footer.php"; ?>-->
 		
 	</body>
 </html>
--- photo.php
+++ photo.php
@@ -1,3 +1,7 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -11,21 +15,19 @@
 		<div class="top">
 			<div class="top_inside">
 				<h3>写真館</h3>
-			<div>
-			<div class="article">
-				<div class="pics">
-					<div class="pic">
-						<img src="./img/pic0.jpg" alt="写真" width="240px" height="150px">
-					</div>
-					<div class="pic">
-						<img src="./img/pic1.jpg" alt="写真" width="240px" height="150px">
-					</div>
-					<div class="pic">
-						<img src="./img/pic2.jpg" alt="写真" width="240px" height="150px">
-					</div>
-				</div>
 			</div>
 		</div>
+			<div class="pics clearfix">
+				<div class="pic">
+					<img src="./img/pic0.jpg" alt="写真" width="240px" height="150px">
+				</div>
+				<div class="pic">
+					<img src="./img/pic1.jpg" alt="写真" width="240px" height="150px">
+				</div>
+				<div class="pic">
+					<img src="./img/pic2.jpg" alt="写真" width="240px" height="150px">
+				</div>
+			</div>
 		
 		<?php include "footer.php"; ?>
 		
