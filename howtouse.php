0abf0c5182749d45e8fd29d8c29a35a77f0d0139
Minori <ararai.mail@gmail.com>
2016-01-06 16:13:09 +0900

    0106

--- howtouse.php
+++ howtouse.php
@@ -15,27 +15,27 @@
 			<div class="article">
 				<h3>はじめてスタンド＆フロントスタンド.wmv</h3>
 				<div class="movie clearfix">
-					<p class="first"><iframe width="280" height="210" src="https://www.youtube.com/embed/IRpBvwE0-Bs" frameborder="0" allowfullscreen></iframe></p>
+					<p class="first"><a href="https://youtu.be/IRpBvwE0-Bs" target="_blank"><img src="./img/movie/beginner.jpg" width="100%" height="auto" alt="はじめてスタンド"></a></p>
 					<p>誰でもわかる、誰でもできるを目指してつくられた｢はじめてスタンド｣の使い方を動画でご紹介</p>
 				</div>
 				<h3>ショートローラースタンドアップ１.AVI</h3>
 				<div class="movie clearfix">
-					<p class="first"><iframe width="280" height="210" src="https://www.youtube.com/embed/6p12UyxATCs" frameborder="0" allowfullscreen></iframe></p>
+					<p class="first"><a href="https://www.youtube.com/watch?v=6p12UyxATCs" target="_blank"><img src="./img/movie/short_up1.jpg" width="100%" height="auto" alt="はじめてスタンド"></a></p>
 					<p>フックボルトと「V受け」を使った｢ショートローラースタンド｣のリフトアップの仕方を動画でご紹介</p>
 				</div>
 				<h3>ショートローラースタンド解除１.AVI</h3>
 				<div class="movie clearfix">
-					<p class="first"><iframe width="280" height="210" src="https://www.youtube.com/embed/--vemO1cQqk" frameborder="0" allowfullscreen></iframe></p>
+					<p class="first"><a href="https://youtu.be/--vemO1cQqk" target="_blank"><img src="./img/movie/short_down1.jpg" width="100%" height="auto" alt="はじめてスタンド"></a></p>
 					<p>フックボルトと「V受け」を使った｢ショートローラースタンド｣のリフトアップの仕方を動画でご紹介</p>
 				</div>
 				<h3>ショートローラースタンドアップ２.AVI</h3>
 				<div class="movie clearfix">
-					<p class="first"><iframe width="280" height="210" src="https://www.youtube.com/embed/89Tn16URajM" frameborder="0" allowfullscreen></iframe></p>
+					<p class="first"><a href="https://youtu.be/89Tn16URajM" target="_blank"><img src="./img/movie/short_up2.jpg" width="100%" height="auto" alt="はじめてスタンド"></a></p>
 					<p>フックボルトと「V受け」を使った｢ショートローラースタンド｣のリフトアップの仕方を動画でご紹介</p>
 				</div>
-				<h3>ショートローラースタンド解除2.AVI.AVI</h3>
+				<h3>ショートローラースタンド解除2.AVI</h3>
 				<div class="movie clearfix">
-					<p class="first"><iframe width="280" height="210" src="https://www.youtube.com/embed/nY3hFtrG-ik" frameborder="0" allowfullscreen></iframe></p>
+					<p class="first"><a href="https://youtu.be/nY3hFtrG-ik" target="_blank"><img src="./img/movie/short_down2.jpg" width="100%" height="auto" alt="はじめてスタンド"></a></p>
 					<p>フックボルトと「V受け」を使った｢ショートローラースタンド｣のリフトアップの仕方を動画でご紹介</p>
 				</div>
 			</div>
--- products.php
+++ products.php
@@ -24,26 +24,38 @@
 				<li>オフ・モタード<br>その他</li>
 				<li>オプション</li>
 			</ul>
+
+<!-------------------リアスタンド---------------------------->
 			<div class="product">
 				<h3>リア</h3>
 				<div class="prod clearfix">
-					<h3>ナローローラースタンド【JT-1052】9,000円(税別)←（右端に寄せる）</h3>
+					<div class="title clearfix">
+						<h3>ナローローラースタンド【JT-1052】</h3>
+						<h3>9,000円(税別)</h3>
+					</div>
 					<div class="prodphoto">
 						<img src="./img/prod/narrow.jpg" width="100%" height="auto" alt="ナローローラースタンド">
 						<p>受け金具は付属していません。用途に合わせて別途お買い求めください。</p>
+						<table>
+							<tr>
+								<td><a href="">受け金具</a></td>
+								<td><a href="">オプション</a></td>
+								<td><a href="">動画</a></td>
+							</tr>
+						</table>
 					</div>
 					<div class="prodtext">
-						<p>50ccから400ccクラスまでの車両に<br>ジャストフィット！</p>
+						<p>50ccから400ccクラスまでの車両にジャストフィット！</p>
 						<p>通常のスタンドよりも幅を50mm狭くしました。別売りのV受け(107A2)またはL受け(104L4)等と組み合わせてご使用ください。</p>
 						<p>**ご注意**<br>オートバイの後部にスタンド本体が長く延びますので、作業中つまづいたりしないようにお気をつけください。</p>
 						<p>カラー</p>
 						<table>
 							<tr>
-								<td class="green">LG</td>
-								<td class="red">RG</td>
-								<td class="orange">QR</td>
-								<td class="white">WT</td>
-								<td class="black">BK</td>
+								<td class="design green">LG</td>
+								<td class="design red">RG</td>
+								<td class="design orange">QR</td>
+								<td class="design white">WT</td>
+								<td class="design black">BK</td>
 							</tr>
 							<tr>
 								<td><input type="submit" value="購入"></td>
@@ -57,23 +69,33 @@
 				</div>
 				
 				<div class="prod clearfix">
-					<h3>ローラースタンド【JT-120】11,000円(税別)←（右端に寄せる）</h3>
+					<div class="title clearfix">
+						<h3>ローラースタンド【JT-120】</h3>
+						<h3>11,000円(税別)</h3>
+					</div>
 					<div class="prodphoto">
 						<img src="./img/prod/roller.jpg" width="100%" height="auto" alt="ナローローラースタンド">
 						<p>受け金具は付属していません。用途に合わせて別途お買い求めください。</p>
+						<table>
+							<tr>
+								<td><a href="">受け金具</a></td>
+								<td><a href="">オプション</a></td>
+								<td><a href="">動画</a></td>
+							</tr>
+						</table>
 					</div>
 					<div class="prodtext">
 						<p>軽い力でらくらくリフト！<br>「ロングローラースタンド」</p>
 						<p>女性でもらくらくリフト可能なロングローラースタンドが定番商品となりました。<br>リッターバイクなどの重量車に特にお勧めです。</p>
 						<p>**ご注意**<br>オートバイの後部にスタンド本体が長く延びますので、作業中つまづいたりしないようにお気をつけください。</p>
-						<p>カラー</p>
+						<p >カラー</p>
 						<table>
 							<tr>
-								<td class="green">LG</td>
-								<td class="red">RG</td>
-								<td class="orange">QR</td>
-								<td class="white">WT</td>
-								<td class="black">BK</td>
+								<td class="design green">LG</td>
+								<td class="design red">RG</td>
+								<td class="design orange">QR</td>
+								<td class="design white">WT</td>
+								<td class="design black">BK</td>
 							</tr>
 							<tr>
 								<td><input type="submit" value="購入"></td>
@@ -87,10 +109,20 @@
 				</div>
 
 				<div class="prod clearfix">
-					<h3>ショートローラースタンド【JT-125】11,000円(税別)←（右端に寄せる）</h3>
+					<div class="title clearfix">
+						<h3>ショートローラースタンド【JT-125】</h3>
+						<h3>11,000円(税別)</h3>
+					</div>
 					<div class="prodphoto">
 						<img src="./img/prod/short.jpg" width="100%" height="auto" alt="ナローローラースタンド">
 						<p>受け金具は付属していません。用途に合わせて別途お買い求めください。</p>
+						<table>
+							<tr>
+								<td><a href="">受け金具</a></td>
+								<td><a href="">オプション</a></td>
+								<td><a href="">動画</a></td>
+							</tr>
+						</table>
 					</div>
 					<div class="prodtext">
 						<p>コンパクト設計で輸送時も安心。スタンドの決定版「ショートローラースタンド」</p>
@@ -99,11 +131,11 @@
 						<p>カラー</p>
 						<table>
 							<tr>
-								<td class="green">LG</td>
-								<td class="red">RG</td>
-								<td class="orange">QR</td>
-								<td class="white">WT</td>
-								<td class="black">BK</td>
+								<td class="design green">LG</td>
+								<td class="design red">RG</td>
+								<td class="design orange">QR</td>
+								<td class="design white">WT</td>
+								<td class="design black">BK</td>
 							</tr>
 							<tr>
 								<td><input type="submit" value="購入"></td>
@@ -117,10 +149,20 @@
 				</div>
 
 				<div class="prod clearfix">
-					<h3>ワイドショートローラースタンド【JT-125P】12,000円(税別)←（右端に寄せる）</h3>
+					<div class="title clearfix">
+						<h3>ワイドショートローラースタンド【JT-125P】</h3>
+						<h3>12,000円(税別)</h3>
+					</div>
 					<div class="prodphoto">
 						<img src="./img/prod/short.jpg" width="100%" height="auto" alt="ナローローラースタンド">
 						<p>受け金具は付属していません。用途に合わせて別途お買い求めください。</p>
+						<table>
+							<tr>
+								<td><a href="">受け金具</a></td>
+								<td><a href="">オプション</a></td>
+								<td><a href="">動画</a></td>
+							</tr>
+						</table>
 					</div>
 					<div class="prodtext">
 						<p>コンパクト設計で輸送時も安心。スタンドの決定版「ショートローラースタンド」</p>
@@ -129,11 +171,11 @@
 						<p>カラー</p>
 						<table>
 							<tr>
-								<td class="green">LG</td>
-								<td class="red">RG</td>
-								<td class="orange">QR</td>
-								<td class="white">WT</td>
-								<td class="black">BK</td>
+								<td class="design green">LG</td>
+								<td class="design red">RG</td>
+								<td class="design orange">QR</td>
+								<td class="design white">WT</td>
+								<td class="design black">BK</td>
 							</tr>
 							<tr>
 								<td><input type="submit" value="購入"></td>
@@ -145,6 +187,258 @@
 						</table>
 					</div>
 				</div>
+
+				<div class="prod clearfix">
+					<div class="title clearfix">
+						<h3>片持ちローラースタンド【JT-136】</h3>
+						<h3>12,500円(税別)</h3>
+					</div>
+					<div class="prodphoto">
+						<img src="./img/prod/jt_136.jpg" width="100%" height="auto" alt="片持ちローラースタンド">
+						<p>受け金具は付属していません。用途に合わせて別途お買い求めください。</p>
+						<table>
+							<tr>
+								<td><a href="">受け金具</a></td>
+								<td><a href="">オプション</a></td>
+								<td><a href="">動画</a></td>
+							</tr>
+						</table>
+					</div>
+					<div class="prodtext">
+						<p>片持ちスイングアームでも安心<br>「片持ちスタンド」</p>
+						<p>支持シャフトの角度を吟味しリフトの軽さと安定性を実現。十分な強度を持ちながらディスプレイスタンドとしても十分鑑賞に堪える美しいパイプワークも魅力。</p>
+						<p><a href="./img/prod/sidearmshaft201404.pdf" target="_blank">適合表</a></p>
+						<p>**ご注意**<br>17インチ車専用品につき18インチ車でご利用になれません。18インチ車へは片持ちスタンドJT-135をご利用ください。</p>
+						<p>カラー</p>
+						<table>
+							<tr>
+								<td class="design red">RG</td>
+								<td class="design white">WT</td>
+								<td class="design black">BK</td>
+							</tr>
+							<tr>
+								<td><input type="submit" value="購入"></td>
+								<td><input type="submit" value="購入"></td>
+								<td><input type="submit" value="購入"></td>
+							</tr>
+						</table>
+					</div>
+				</div>
+
+				<div class="prod clearfix">
+					<div class="title clearfix">
+						<h3>片持ちローラースタンド【JT-137】</h3>
+						<h3>12,500円(税別)</h3>
+					</div>
+					<div class="prodphoto">
+						<img src="" width="100%" height="200px" alt="片持ちローラースタンド">
+						<p>受け金具は付属していません。用途に合わせて別途お買い求めください。</p>
+						<table>
+							<tr>
+								<td><a href="">受け金具</a></td>
+								<td><a href="">オプション</a></td>
+								<td><a href="">動画</a></td>
+							</tr>
+						</table>
+					</div>
+					<div class="prodtext">
+						<p>片持ちスイングアームでも安心<br>「片持ちスタンド」</p>
+						<p>片持ちスタンドJT-136を右スイングアーム用にしたものです。現在適合車種はBMW各車とホンダRS250のみです。</p>
+						<p>**ご注意**<br>BMW専用品とお考え下さい。なお、BMW K1200R/Sには適合いたしません。</p>
+						<p>カラー</p>
+						<table>
+							<tr>
+								<td class="design red">RG</td>
+								<td class="design white">WT</td>
+								<td class="design black">BK</td>
+							</tr>
+							<tr>
+								<td><input type="submit" value="購入"></td>
+								<td><input type="submit" value="購入"></td>
+								<td><input type="submit" value="購入"></td>
+							</tr>
+						</table>
+					</div>
+				</div>
+
+				<div class="prod clearfix">
+					<div class="title clearfix">
+						<h3>片持ちローラースタンド【JT-135】</h3>
+						<h3>9,500円(税別)</h3>
+					</div>
+					<div class="prodphoto">
+						<img src="./img/prod/jt_135.jpg" width="100%" height="auto" alt="片持ちローラースタンド">
+						<p>受け金具は付属していません。用途に合わせて別途お買い求めください。</p>
+						<table>
+							<tr>
+								<td><a href="">受け金具</a></td>
+								<td><a href="">オプション</a></td>
+								<td><a href="">動画</a></td>
+							</tr>
+						</table>
+					</div>
+					<div class="prodtext">
+						<p>片持ちスイングアームでも安心<br>「片持ちスタンド」</p>
+						<p>片持ちローラースタンドJT-136のローラーを省いたものです。抜群の安定性でディスプレイに好適。リーズナブルな価格も魅力。18インチ対応の為リフト量が大きく、17インチ車を懸架する場合、他の製品に比べて少々重たく感じられる場合があります。</p>
+						<p>カラー</p>
+						<table>
+							<tr>
+								<td class="design white">WT</td>
+							</tr>
+							<tr>
+								<td><input type="submit" value="購入"></td>
+							</tr>
+						</table>
+					</div>
+				</div>
+				
+<!---------------フロントスタンド---------------->
+				<h3>フロント</h3>
+				<div class="prod clearfix">
+					<div class="title clearfix">
+						<h3>フロントスタンド【JT-116】</h3>
+						<h3>19,500円(税別)</h3>
+					</div>
+					<div class="prodphoto">
+						<img src="./img/prod/jt_116.jpg" width="100%" height="auto" alt="フロントスタンド">
+						<table>
+							<tr>
+								<td><a href="">受け金具</a></td>
+								<td><a href="">オプション</a></td>
+								<td><a href="">動画</a></td>
+							</tr>
+						</table>
+					</div>
+					<div class="prodtext">
+						<p>安定感で選ぶなら「フロントスタンド」</p>
+						<p>ステアリングステムにφ13ｍｍ以上の穴があいている、アメリカン・オフロード以外の車種であればほぼ全機種をリフト可能なフロントスタンドです。</p>
+						<p>**ご注意**<br>構造上リフトしたまま三又を外す事は出来ません。また一部制約のある車種があります。</p>
+						<p>カラー</p>
+						<table>
+							<tr>
+								<td class="design green">LG</td>
+								<td class="design red">RG</td>
+								<td class="design orange">QR</td>
+								<td class="design white">WT</td>
+								<td class="design black">BK</td>
+							</tr>
+							<tr>
+								<td><input type="submit" value="購入"></td>
+								<td><input type="submit" value="購入"></td>
+								<td><input type="submit" value="購入"></td>
+								<td><input type="submit" value="購入"></td>
+								<td><input type="submit" value="購入"></td>
+							</tr>
+						</table>
+					</div>
+				</div>
+
+				<div class="prod clearfix">
+					<div class="title clearfix">
+						<h3>フォークアップスタンド【JT-113】</h3>
+						<h3>11,750円(税別)</h3>
+					</div>
+					<div class="prodphoto">
+						<img src="./img/prod/jt_113.jpg" width="100%" height="auto" alt="フォークアップスタンド">
+						<table>
+							<tr>
+								<td><a href="">受け金具</a></td>
+								<td><a href="">オプション</a></td>
+								<td><a href="">動画</a></td>
+							</tr>
+						</table>
+					</div>
+					<div class="prodtext">
+						<p>優れた汎用性であらゆるマシンをリフトする「フォークアップスタンド」</p>
+						<p>フォークボトムにφ8ｍｍ以上の穴があいていればほぼ全ての車種をリフト可能なフロントスタンドです。<br>左右の幅調整は無段階・左右とも独立して高さ調整も無段階です。（実用新案登録　第3074529号）</p>
+						<p>**ご注意**<br>構造上リフトしたままフォークを外す事は出来ません。フォークのOHにはフロントスタンドJT-116をお勧めいたします。</p>
+						<p>カラー</p>
+						<table>
+							<tr>
+								<td class="design red">RG</td>
+								<td class="design white">WT</td>
+								<td class="design black">BK</td>
+							</tr>
+							<tr>
+								<td><input type="submit" value="購入"></td>
+								<td><input type="submit" value="購入"></td>
+								<td><input type="submit" value="購入"></td>
+							</tr>
+						</table>
+					</div>
+				</div>
+
+				<div class="prod clearfix">
+					<div class="title clearfix">
+						<h3>DUCATI専用フロントスタンド【JT-113DRD】</h3>
+						<h3>12,000円(税別)</h3>
+					</div>
+					<div class="prodphoto">
+						<img src="./img/prod/jt_113drd.jpg" width="100%" height="auto" alt="DUCATI専用フロントスタンド">
+						<table>
+							<tr>
+								<td><a href="">受け金具</a></td>
+								<td><a href="">オプション</a></td>
+								<td><a href="">動画</a></td>
+							</tr>
+						</table>
+					</div>
+					<div class="prodtext">
+						<p>専用設計ならではの使いやすさ</p>
+						<p>ステアリングステムに穴がないDUCATI専用品。キャリパーサポートで支持しますので安定感も抜群。<br>適応車種は<a href="">こちら</a>からご確認下さい。</p>
+						<p>カラー</p>
+						<table>
+							<tr>
+								<td class="design red">RG</td>
+							</tr>
+							<tr>
+								<td><input type="submit" value="購入"></td>
+							</tr>
+						</table>
+					</div>
+				</div>
+
+<!---------------ミニスタンド---------------->
+				<h3>ミニ</h3>
+				<div class="info">
+					<h3>お知らせ</h3>
+					<p>
+					ミニバイク専用リアスタンドは販売終了しました。<br>
+					後継モデルは<a href="">ナローローラースタンド【JT-1052】</a>です。<br><br>
+					</p>
+				</div>
+				
+				<div class="prod clearfix">
+					<div class="title clearfix">
+						<h3>ミニフロントスタンド【JT-116S】</h3>
+						<h3>16,000円(税別)</h3>
+					</div>
+					<div class="prodphoto">
+						<img src="./img/prod/jt_116s.jpg" width="100%" height="auto" alt="フロントスタンド">
+						<table>
+							<tr>
+								<td><a href="">受け金具</a></td>
+								<td><a href="">動画</a></td>
+							</tr>
+						</table>
+					</div>
+					<div class="prodtext">
+						<p>ミニバイク専用設計「ミニフロントスタンド」</p>
+						<p>ステアリングステムにφ13ｍｍ以上の穴があいている、アメリカン・オフロード以外の車種であればほぼ全機種をリフト可能なフロントスタンドです。</p>
+						<p>**ご注意**<br>KSR50/80/110やエイプ50/100に使用する場合別売りブラケットが必要です。<br>（XR50/100モタード専用ブラケットは販売終了しました）<br>構造上リフトしたまま三又を外す事は出来ません。</p>
+						<p>カラー</p>
+						<table>
+							<tr>
+								<td class="design red">RG</td>
+								<td class="design white">WT</td>
+							</tr>
+							<tr>
+								<td><input type="submit" value="購入"></td>
+							</tr>
+						</table>
+					</div>
+				</div>
+
 
 				
 			</div>
